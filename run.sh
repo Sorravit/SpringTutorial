ae1c50712bf9ea1564258ccefd09c7d2c35ea7c5
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-06-06 00:15:57 +0700

    Create database via docker, run the application also remove flyway for the moment

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-goal_hello(){
-    echo "Hello, from the other side!"
+goal_start_database(){
+    docker run --detach --env MYSQL_ROOT_PASSWORD=sorravit --env MYSQL_USER=sorravit --env MYSQL_PASSWORD=sorravit --env MYSQL_DATABASE=big --name mysql --publish 3302:3306 mysql
 }
 if type -t "goal_$1" &>/dev/null; then
   goal_$1 ${@:-2}
 else
     echo "usage: $0 <goal>
     goal:
-        hello                     -- say hello back so the programmer is not lonely
+        start_database                     -- start database from mysql docker image
         "
   exit 1
 fi
