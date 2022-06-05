4f2a9ab05e8b7d273992b3c58b5c4071a34d379a
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-06-06 00:07:28 +0700

    init, not working of casue

---
+++ src/test/java/sorravit/sprinttutorial/SprintTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package sorravit.sprinttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
