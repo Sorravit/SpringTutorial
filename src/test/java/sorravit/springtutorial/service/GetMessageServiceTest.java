54ab83d91a1a69590db1547b737e50a936498409
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-06-18 00:24:43 +0700

    Add test for service, it's not thing much for now because it's just returning the static text

---
+++ src/test/java/sorravit/springtutorial/service/GetMessageServiceTest.java
@@ -0,0 +1,14 @@
+package sorravit.springtutorial.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetMessageServiceTest {
+
+    @Test
+    void getMessage() {
+        GetMessageService getMessageService = new GetMessageService();
+        assertEquals("Hello World!", getMessageService.getMessage());
+    }
+}
