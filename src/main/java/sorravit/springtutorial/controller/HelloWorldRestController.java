c5b4ff9defa71770ea91a3fd2a8f047b4bcf734b
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-07-16 18:08:49 +0700

    Spring cache, without redis

--- src/main/java/sorravit/springtutorial/controller/HelloWorldRestController.java
+++ src/main/java/sorravit/springtutorial/controller/HelloWorldRestController.java
@@ -2,6 +2,7 @@ package sorravit.springtutorial.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import sorravit.springtutorial.service.GetMessageService;
 
@@ -9,6 +10,7 @@ import sorravit.springtutorial.service.GetMessageService;
 public class HelloWorldRestController {
     private final GetMessageService getMessageService;
 
+    @Autowired
     public HelloWorldRestController(GetMessageService getMessageService) {
         this.getMessageService = getMessageService;
     }
@@ -16,5 +18,10 @@ public class HelloWorldRestController {
     @GetMapping("/")
     public String greeting() {
         return getMessageService.getMessage();
+    }
+
+    @GetMapping("/{name}")
+    public String greetingWithName(@PathVariable String name) {
+        return getMessageService.getMessage(name);
     }
 }
--- src/main/java/sorravit/springtutorial/service/GetMessageService.java
+++ src/main/java/sorravit/springtutorial/service/GetMessageService.java
@@ -1,10 +1,20 @@
 package sorravit.springtutorial.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableCaching
+@Slf4j
 public class GetMessageService {
     public String getMessage() {
         return "Hello World!";
+    }
+    @Cacheable("message")
+    public String getMessage(String name) {
+        log.info("Cache miss for message with name: " + name);
+        return "Hello " + name + "!";
     }
 }
