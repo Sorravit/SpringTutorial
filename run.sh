5bb392a4eb032a14b39ec69649fe8623ddd62c32
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-07-16 13:54:42 +0700

    Add redis docker, get ready for caching

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REDIS_DOCKER_NAME="redis"
 DATABASE_DOCKER_NAME="mysql"
 
 goal_database_start() {
@@ -10,6 +11,13 @@ goal_database_stop() {
 goal_database_migrate() {
   ./gradlew flywayMigrate
 }
+goal_redis_start() {
+  docker run -d --rm --name ${REDIS_DOCKER_NAME} -e "ALLOW_EMPTY_PASSWORD=false" -e "REDIS_PASSWORD=Password123" -p6379:6379 bitnami/redis
+}
+goal_redis_stop() {
+  docker kill ${REDIS_DOCKER_NAME}
+}
+
 if type -t "goal_$1" &>/dev/null; then
   goal_$1 ${@:-2}
 else
@@ -18,6 +26,8 @@ else
         database_start                      -- start database from mysql docker image
         database_stop                       -- stop database from mysql docker image
         database_migrate                    -- migrate database using flyway migration
+        redis_start                         -- start redis from redis docker image
+        redis_stop                          -- stop redis docker
         "
   exit 1
 fi
