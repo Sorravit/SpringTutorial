8a009c225abc14f434a067f482a27ea2c9d46311
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2022-07-16 13:53:53 +0700

    Add migration to run.sh for easy access

--- run.sh
+++ run.sh
@@ -1,12 +1,23 @@
-goal_start_database(){
-    docker run --detach --env MYSQL_ROOT_PASSWORD=sorravit --env MYSQL_USER=sorravit --env MYSQL_PASSWORD=sorravit --env MYSQL_DATABASE=big --name mysql --publish 3302:3306 mysql
+#!/bin/bash
+DATABASE_DOCKER_NAME="mysql"
+
+goal_database_start() {
+  docker run --detach --env MYSQL_ROOT_PASSWORD=sorravit --env MYSQL_USER=sorravit --env MYSQL_PASSWORD=sorravit --env MYSQL_DATABASE=big --name ${DATABASE_DOCKER_NAME} --publish 3302:3306 mysql
+}
+goal_database_stop() {
+  docker stop ${DATABASE_DOCKER_NAME}
+}
+goal_database_migrate() {
+  ./gradlew flywayMigrate
 }
 if type -t "goal_$1" &>/dev/null; then
   goal_$1 ${@:-2}
 else
-    echo "usage: $0 <goal>
+  echo "usage: $0 <goal>
     goal:
-        start_database                     -- start database from mysql docker image
+        database_start                      -- start database from mysql docker image
+        database_stop                       -- stop database from mysql docker image
+        database_migrate                    -- migrate database using flyway migration
         "
   exit 1
 fi
