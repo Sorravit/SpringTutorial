f087a7b968671440d95b7ec9e99705d14e52a179
Big <sorravit16758@gmail.com>
2025-08-15 03:17:30 +0700

    Update run.sh to rename Docker containers and add development environment goals

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-REDIS_DOCKER_NAME="redis"
-DATABASE_DOCKER_NAME="mysql"
+REDIS_DOCKER_NAME="spring-tutorial-redis"
+DATABASE_DOCKER_NAME="spring-tutorial-mysql"
 
 goal_database_start() {
   docker run --detach --env MYSQL_ROOT_PASSWORD=sorravit --env MYSQL_USER=sorravit --env MYSQL_PASSWORD=sorravit --env MYSQL_DATABASE=big --name ${DATABASE_DOCKER_NAME} --publish 3302:3306 mysql
@@ -17,12 +17,22 @@ goal_redis_start() {
 goal_redis_stop() {
   docker kill ${REDIS_DOCKER_NAME}
 }
+goal_start_dev_env() {
+  goal_database_start
+  goal_redis_start
+}
+goal_stop_dev_env() {
+  goal_database_stop
+  goal_redis_stop
+}
 
 if type -t "goal_$1" &>/dev/null; then
   goal_$1 ${@:-2}
 else
   echo "usage: $0 <goal>
     goal:
+        start_dev_env                       -- start development environment
+        stop_dev_env                        -- stop development environment
         database_start                      -- start database from mysql docker image
         database_stop                       -- stop database from mysql docker image
         database_migrate                    -- migrate database using flyway migration
